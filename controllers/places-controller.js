91206327cc8876cc1a3200bcbf4f602c4e82c524
Jason Collum <jasoncollum@gmail.com>
2020-03-03 11:02:17 -0600

    validate updatePlace

--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -60,6 +60,11 @@ const createPlace = (req, res, next) => {
 }
 
 const updatePlace = (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        throw new HttpError('Invalid input, please check your data', 422);
+    }
+
     const { title, description } = req.body;
     const placeId = req.params.pid;
 
