1a25723aeb315addc5e4d878905b263534a59f8f
Jason Collum <jasoncollum@gmail.com>
2020-03-08 17:28:06 -0500

    generate token at signup

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,5 +1,6 @@
 const { validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const HttpError = require('../models/http-error');
 const User = require('../models/user');
@@ -65,7 +66,26 @@ const signup = async (req, res, next) => {
         );
     }
 
-    res.status(201).json({ user: createdUser.toObject({ getters: true }) });
+    let token;
+    try {
+        token = jwt.sign({
+            userId: createdUser.id,
+            email: createdUser.email
+        },
+            'supersecret',
+            { expiresIn: '1h' }
+        );
+    } catch (error) {
+        return next(
+            new HttpError('Sign up failed', 500)
+        );
+    }
+
+    res.status(201).json({
+        userId: createdUser.id,
+        email: createdUser.email,
+        token
+    });
 };
 
 const login = async (req, res, next) => {
